e60727d00a955241987f7c7be4e262cf82ae146b
Flori <39242991+bitfl0wer@users.noreply.github.com>
2025-04-06 14:31:02 +0200

    Blue (#4)
    blue. replaces the yellow in most places

--- src/components/Contribute.tsx
+++ src/components/Contribute.tsx
@@ -6,7 +6,7 @@ export function Contribute(): JSX.Element {
         <section className="bg-poly-white">
             <div className="-mb-4 hidden lg:block">
                 <Wave
-                    fill="#e1f852"
+                    fill="#3577ff"
                     paused={false}
                     options={{
                         height: 80,
@@ -18,7 +18,7 @@ export function Contribute(): JSX.Element {
             </div>
             <div className="-mb-4 lg:hidden">
                 <Wave
-                    fill="#e1f852"
+                    fill="#3577ff"
                     paused={false}
                     options={{
                         height: 100,
@@ -31,34 +31,34 @@ export function Contribute(): JSX.Element {
             <div className="bg-poly-green-2 h-[50vh] w-full">
                 <div className="bg-poly-green-2 grid grid-cols-10 md:grid-cols-12 h-full max-w-7xl px-4 mx-auto place-items-center">
                     <div className="col-start-1 col-span-10 md:col-start-3 md:col-span-8">
-                        <h2 className="text-4xl md:text-5xl font-bold text-poly-black text-center">
+                        <h2 className="text-4xl md:text-5xl font-bold text-poly-white text-center">
                             Join the network of creators
                         </h2>
-                        <h2 className="text-2xl md:text-4xl text-poly-black pt-2 md:text-center">
+                        <h2 className="text-2xl md:text-4xl text-poly-white pt-2 md:text-center">
                             Contributions are always welcome!
                         </h2>
                         <div className="flex flex-col-reverse md:flex-row md:justify-between mt-10 md:gap-8">
-                            <p className="text-poly-black text-lg text-right md:text-left">
+                            <p className="text-poly-white text-lg text-right md:text-left">
                                 Develop{" "}
-                                <span className="text-poly-black font-bold">
+                                <span className="text-poly-white font-bold">
                                     for or with polyproto.
                                 </span>
                             </p>
                             <div className="flex flex-col items-end md:items-start justify-center gap-y-3">
-                                <div className="flex border-b border-poly-black w-44 md:w-auto">
+                                <div className="flex border-b border-poly-white w-44 md:w-auto">
                                     <a
                                         target="_blank"
-                                        className="text-poly-black text-lg flex items-center gap-3 md:text-xl hover:no-underline no-underline hover:text-poly-black hover:scale-105 transition ease-in-out duration-500 hover:drop-shadow-sm"
+                                        className="text-poly-white text-lg flex items-center gap-3 md:text-xl hover:no-underline no-underline hover:text-poly-white hover:scale-105 transition ease-in-out duration-500 hover:drop-shadow-sm"
                                         href="https://github.com/polyphony-chat/polyproto-rs"
                                     >
                                         <ArrowRight className="size-5" />
                                         View on GitHub
                                     </a>
                                 </div>
-                                <div className="flex border-b border-poly-black w-44 md:w-auto">
+                                <div className="flex border-b border-poly-white w-44 md:w-auto">
                                     <a
                                         target="_blank"
-                                        className="text-poly-black text-lg flex items-center gap-3 md:text-xl hover:no-underline no-underline hover:text-poly-black hover:scale-105 transition ease-in-out duration-500 hover:drop-shadow-sm"
+                                        className="text-poly-white text-lg flex items-center gap-3 md:text-xl hover:no-underline no-underline hover:text-poly-white hover:scale-105 transition ease-in-out duration-500 hover:drop-shadow-sm"
                                         href="/docs/contribute"
                                     >
                                         <ArrowRight className="size-5" />
@@ -70,7 +70,7 @@ export function Contribute(): JSX.Element {
                     </div>
                 </div>
                 <Wave
-                    fill="#e1f852"
+                    fill="#3577ff"
                     paused={false}
                     className="rotate-180 -mt-1"
                     options={{
