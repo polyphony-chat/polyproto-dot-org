e5143c57cb67125c5a695cf714a550f678f57b69
bitfl0wer <florian@pro-weber.com>
2025-06-16 22:09:45 +0200

    chore: add versioning notice
    chore: run prettier, fix markdownlint violations
    
    chore: pre-commit autoupdate

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,36 +1,41 @@
-import { Anchor } from "./Anchor";
-import ArrowRight from "./icons/ArrowRight";
+import { Anchor } from "./Anchor"
+import ArrowRight from "./icons/ArrowRight"
 
 export function Hero(): JSX.Element {
-  return (
-    <section className="bg-poly-black min-h-[70vh] lg:min-h-[75vh] md:h-[70vh] py-10 px-4 md:py-14 overflow-x-hidden">
-      <div className="bg-poly-black grid grid-cols-10 h-full max-w-7xl mx-auto md:px-0 lg:grid-cols-12">
-        <div className="col-start-1 col-span-10 md:col-span-12">
-          <div className="grid grid-cols-12 fade-in md:py-0">
-            <div className="col-start-1 col-end-13 md:col-span-8 fade-in-left">
-              <h3 className="text-center text-poly-green-1 text-lg md:text-2xl md:text-left xl:text-3xl fade-in-subtitle">
-                A refreshingly simple <br />
-                decentralised, federated protocol
-              </h3>
-              <h2 className="text-white text-center text-[2.70rem] font-bold md:text-left md:text-[4.1rem] lg:text-[5.5rem] xl:text-[7rem]">
-                Transforming
-              </h2>
+    return (
+        <section className="bg-poly-black min-h-[70vh] lg:min-h-[75vh] md:h-[70vh] py-10 px-4 md:py-14 overflow-x-hidden">
+            <div className="bg-poly-black grid grid-cols-10 h-full max-w-7xl mx-auto md:px-0 lg:grid-cols-12">
+                <div className="col-start-1 col-span-10 md:col-span-12">
+                    <div className="grid grid-cols-12 fade-in md:py-0">
+                        <div className="col-start-1 col-end-13 md:col-span-8 fade-in-left">
+                            <h3 className="text-center text-poly-green-1 text-lg md:text-2xl md:text-left xl:text-3xl fade-in-subtitle">
+                                A refreshingly simple <br />
+                                decentralised, federated protocol
+                            </h3>
+                            <h2 className="text-white text-center text-[2.70rem] font-bold md:text-left md:text-[4.1rem] lg:text-[5.5rem] xl:text-[7rem]">
+                                Transforming
+                            </h2>
+                        </div>
+                        <div className="col-start-1 col-end-13 font-bold md:flex md:items-center md:col-start-2 fade-in-right">
+                            <h2 className="text-center text-8xl bg-clip-text text-transparent bg-gradient-to-r from-[#afa5f4] to-[#8471fe] md:text-[4.1rem] lg:text-[5.5rem] xl:text-[7rem]">
+                                online
+                            </h2>
+                            <h2 className="text-white text-center text-[2.70rem] md:text-[4.1rem] lg:text-[5.5rem] xl:text-[7rem]">
+                                &nbsp;communication
+                            </h2>
+                        </div>
+                        <div className="col-start-3 col-span-8 md:col-start-5 md:col-span-4 pt-20 md:pt-16 xl:pt-10">
+                            <Anchor
+                                href="/docs/intro"
+                                className="uppercase font-semibold flex justify-center items-center gap-5 md:text-xl xl:text-2xl anchor-shadow transition ease-in-out duration-300"
+                            >
+                                Get Started
+                                <ArrowRight className="size-5 md:size-6" />
+                            </Anchor>
+                        </div>
+                    </div>
+                </div>
             </div>
-            <div className="col-start-1 col-end-13 font-bold md:flex md:items-center md:col-start-2 fade-in-right">
-              <h2 className="text-center text-8xl bg-clip-text text-transparent bg-gradient-to-r from-[#afa5f4] to-[#8471fe] md:text-[4.1rem] lg:text-[5.5rem] xl:text-[7rem]">
-                online
-              </h2>
-              <h2 className="text-white text-center text-[2.70rem] md:text-[4.1rem] lg:text-[5.5rem] xl:text-[7rem]">&nbsp;communication</h2>
-            </div>
-            <div className="col-start-3 col-span-8 md:col-start-5 md:col-span-4 pt-20 md:pt-16 xl:pt-10">
-            <Anchor href="/docs/intro" className="uppercase font-semibold flex justify-center items-center gap-5 md:text-xl xl:text-2xl anchor-shadow transition ease-in-out duration-300">
-              Get Started
-              <ArrowRight className="size-5 md:size-6"/>
-            </Anchor>
-            </div>
-          </div>
-        </div>
-      </div>
-    </section>
-  );
+        </section>
+    )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,25 +1,25 @@
-import Layout from "@theme/Layout";
-import type { ReactNode } from "react";
+import Layout from "@theme/Layout"
+import type { ReactNode } from "react"
 
-import { About } from "../components/About";
-import { Contribute } from "../components/Contribute";
-import { GetStarted } from "../components/GetStarted";
-import { Hero } from "../components/Hero";
-import { Product } from "../components/Product";
+import { About } from "../components/About"
+import { Contribute } from "../components/Contribute"
+import { GetStarted } from "../components/GetStarted"
+import { Hero } from "../components/Hero"
+import { Product } from "../components/Product"
 
 export default function Home(): ReactNode {
-  return (
-    <Layout
-      title="Transforming Online Communication"
-      description="The polyproto federation protocol <head />"
-    >
-      <main>
-        <Hero />
-        <About />
-        <Contribute />
-        <Product />
-        <GetStarted />
-      </main>
-    </Layout>
-  );
+    return (
+        <Layout
+            title="Transforming Online Communication"
+            description="The polyproto federation protocol <head />"
+        >
+            <main>
+                <Hero />
+                <About />
+                <Contribute />
+                <Product />
+                <GetStarted />
+            </main>
+        </Layout>
+    )
 }
